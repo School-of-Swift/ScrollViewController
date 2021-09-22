274f07c6c4a32d1c8edf1d2f5260aee129df006f
Frank Courville <hello@frankcourville.com>
2021-09-22 14:05:09 -0400

    Correct name

--- Sources/ScrollViewController/ScrollViewController.swift
+++ Sources/ScrollViewController/ScrollViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-public class StackViewController: UIViewController {
+open class ScrollViewController: UIViewController {
     public override func loadView() {
         let view = UIView()
 
